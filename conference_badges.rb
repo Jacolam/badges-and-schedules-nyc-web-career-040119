b3b81555ee473618f08072271c7c12411999d686
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-17 17:30:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,15 +15,10 @@ end
 
 def assign_rooms(list_of_names)
   room_assignment = []
-  
   list_of_names.each_with_index { |name , index|
-  
  room_assignment << "Hello, #{name}! You'll be assigned to room #{index+1}!"
- 
 }
-
 room_assignment
-
 end
   
 def printer(list_of_names)
