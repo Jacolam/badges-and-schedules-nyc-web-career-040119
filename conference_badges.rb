e99ffdb7deef06c53fef65e1b6146ff97cd58ecc
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-17 16:59:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(list_of_names)
+  
  badge_messages = []
+ 
  list_of_names.each do |list_of_names|
    badge_messages.push badge_maker(list_of_names)
  end
+ 
  badge_messages
+ 
 end
